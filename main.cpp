9cf4e613ab6da1b2e6b6a6f6befdf06b9205c3ae
Pako2425 <patryk.kocinski340@gmail.com>
2022-08-16 21:44:38 +0200

    SFML pierwsze kroki: tworzenie okna, wyswietlanie tekstu, odczyt klawiatury

--- main.cpp
+++ main.cpp
@@ -2,21 +2,42 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
+    sf::RenderWindow window(sf::VideoMode(400,400), "game_window");
+    window.setFramerateLimit(60);
+    sf::Font font;
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(30);
+    text.setFillColor(sf::Color::Green);
+    if(!font.loadFromFile("font_files/atwriter.ttf"))
+    {
+        return EXIT_FAILURE;
+    }
+    while(window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
         }
-
         window.clear();
-        window.draw(shape);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            text.setString("Move left!");
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            text.setString("Move right!");
+        }
+        else
+        {
+            text.setString(" ");
+        }
+        text.setPosition(50, 100);
+        window.draw(text);
         window.display();
     }
 
