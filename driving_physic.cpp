4877e70062e6d70a0539bb206688a2ac7fb357f2
Pako2425 <patryk.kocinski340@gmail.com>
2022-10-28 23:53:20 +0200

    turning radius function repaired

--- driving_physic.cpp
+++ driving_physic.cpp
@@ -14,26 +14,21 @@ Driving_Physic::~Driving_Physic()
 void Driving_Physic::calculateRadiusOfCarTurningCircle(const Car& anyCar)
 {
     double Beta = 0.0;
-    if(anyCar.getSteerAngle() < -0.6)
+    if(std::abs(anyCar.getSteerAngle()) > 0.6)
     {
-        Beta = 90.0+anyCar.getSteerAngle();
-        this->turningRadius = anyCar.getLength()*tan(Beta*M_PI/180);
-    }
-    else if(anyCar.getSteerAngle() > 0.6)
-    {
-        Beta = 90.0-anyCar.getSteerAngle();;
+        Beta = 90.0-std::abs(anyCar.getSteerAngle());
         this->turningRadius = anyCar.getLength()*tan(Beta*M_PI/180);
     }
     else
     {
-        this->turningRadius = -1.0;
+        this->turningRadius = 0.0;
     }
 }
 
 void Driving_Physic::calculateCarRotationAngle(const Car& anyCar)
 {
     double dAlfa = 0.0;
-    if(this->turningRadius >= 0)
+    if(this->turningRadius > 0)
     {
         dAlfa = anyCar.getVelocity()*this->dt/this->turningRadius;
         this->dCarRotationAngle = dAlfa*180/M_PI;
